327e235f796d339242b675e6f7aa37b09cffdbfb
sanderhelleso <sanderhelleso@hotmail.com>
2019-09-28 00:24:01 +0200

    Updated api support and added back button from Login

--- app/app/src/main/java/com/iifym/LoginActivity.java
+++ app/app/src/main/java/com/iifym/LoginActivity.java
@@ -3,6 +3,7 @@ package com.iifym;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
