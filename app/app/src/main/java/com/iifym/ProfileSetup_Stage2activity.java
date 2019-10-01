87fe9abc61da63f50f4aa5d3dab8c32172a83e62
sanderhelleso <sanderhelleso@hotmail.com>
2019-10-02 01:16:46 +0200

    Added first seekbar to stage 2

--- app/app/src/main/java/com/iifym/ProfileSetup_Stage2activity.java
+++ app/app/src/main/java/com/iifym/ProfileSetup_Stage2activity.java
@@ -3,6 +3,8 @@ package com.iifym;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ProfileSetup_Stage2activity extends AppCompatActivity {
 
@@ -10,5 +12,37 @@ public class ProfileSetup_Stage2activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_setup__stage2);
+
+        initCurrentWeightSeekBar();
+    }
+
+    private void initCurrentWeightSeekBar() {
+        final int MIN = 40;
+        final int MAX = 180;
+
+        final TextView textView = findViewById(R.id.currentWeight_text);
+        SeekBar seekBar = findViewById(R.id.currentWeight_seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (progress <= MIN) {
+                    progress = MIN + progress;
+                }
+
+                StringBuilder sb = new StringBuilder(String.valueOf(progress));
+                textView.setText(sb.append(progress == MAX ? "+" : "").toString());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
