c17b82562f6b64ae165b20c537e3ffe3b23272cd
sanderhelleso <sanderhelleso@hotmail.com>
2019-10-08 04:59:16 +0200

    Added tranistion from home -> settings and back

--- app/app/src/main/java/com/iifym/HomeActivity.java
+++ app/app/src/main/java/com/iifym/HomeActivity.java
@@ -2,7 +2,11 @@ package com.iifym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void gotoSettings(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
+        overridePendingTransition(R.anim.enter, R.anim.fade_out);
     }
 }
