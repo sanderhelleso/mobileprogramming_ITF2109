e2c3b73e99de7bd65308a7a58e737b543be1d9f5
sanderhelleso <sanderhelleso@hotmail.com>
2019-10-29 16:55:15 +0100

    Updated icons on bottombar

--- app/app/src/main/java/com/iifym/HomeActivity.java
+++ app/app/src/main/java/com/iifym/HomeActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
