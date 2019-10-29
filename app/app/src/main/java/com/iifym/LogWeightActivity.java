52f332e95be9967dc7ebebade0e665d00574ac4f
sanderhelleso <sanderhelleso@hotmail.com>
2019-10-29 15:26:09 +0100

    Removed unneeded check

--- app/app/src/main/java/com/iifym/LogWeightActivity.java
+++ app/app/src/main/java/com/iifym/LogWeightActivity.java
@@ -56,46 +56,7 @@ public class LogWeightActivity extends AppCompatActivity {
             return false;
         }
 
-        final double SAFE_WEEKLY_LOSS = 0.90718474;
-        double MAX_OFFSET = 100;//Math.ceil(User.getWeightLogs().getDaysSinceLastLogged() / 7.0) * (SAFE_WEEKLY_LOSS * 3);
-
-        double weightInputValue = Double.parseDouble(inputVal);
-        double currToMax = currentWeight + MAX_OFFSET;
-        double currToLow = currentWeight - MAX_OFFSET;
-
-        boolean notValidInput = weightInputValue > currToMax || weightInputValue < currToLow;
-
-        if (notValidInput) {
-            String maxVal = String.format("%.2f", weightInputValue >  currToMax ? currToMax : currToLow);
-            String limitMsg = "\nThe maximum weight since your last tracked log should not be more or less than " +
-                    maxVal + "kg to progress in a healthy speed.\n\nDo you want to proceed?\n";
-
-            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Confirm add log");
-            builder.setMessage(limitMsg)
-                    .setPositiveButton("Yes, Add to log", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {
-                            addLog();
-                        }
-                    })
-                    .setNegativeButton("No, Cancel", new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int id) {}
-                    });
-
-            final AlertDialog alertDialog = builder.create();
-            alertDialog.setOnShowListener( new DialogInterface.OnShowListener() {
-                @Override
-                public void onShow(DialogInterface arg0) {
-                    int color = getResources().getColor(R.color.indigo);
-                    alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(color);
-                    alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(color);
-                }
-            });
-
-            alertDialog.show();
-        }
-
-        return !notValidInput;
+        return true;
     }
 
     public void logWeight(View view) {
